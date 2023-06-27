a81a66b4f74f967c6ca30e8a13ea8004ce79b43a
Ranel John E. Abanto <63227768+radabanto@users.noreply.github.com>
2023-06-27 12:34:59 +0930

    Add project files.

---
+++ LoginAuthProject/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using LoginAuthProject.Services;
+using LoginAuthProject.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace LoginAuthProject
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ LoginAuthProject/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using LoginAuthProject.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace LoginAuthProject.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
